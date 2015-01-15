7c2afbe3542cfd7ee3b7e486ea3d7448d007ad0f
jswithalex <alexanderramsdell@gmail.com>
2015-01-15 17:36:18 -0500

    Update console_exercises.js

--- alex/console_exercises.js
+++ alex/console_exercises.js
@@ -12,21 +12,46 @@ x
 
 y = Math.sqrt(x)
 
+y
+
 x = Math.sqrt(x)
+
+x
 
 alert('x is ' + x)
 
 console.log('x is ' + x);
 
-console.log('x + x = ' + x + x )
+console.log('2 times x is ' + 2x)
 
-console.log('x + x = ' + 2x )
+console.log('2 times x is ' + 2*x)
 
-console.log('x + x = ' + 2*x )
+console.log('x + x is ' + x + x )
 
 console.log('x + x = ' + (x + x) )
 
-'x'
+var firstName = 'alex'
 
-x
+var lastName = 'alex'
+
+var fullName = 'alex' + 'ramsdell'
+
+var fullName = 'alex' + ' ' + 'ramsdell'
+
+x = 4
+
+y = '4'
+
+y == x
+
+y === x
+
+y = x
+
+y === x
+
+if (x === y) console.log('x and y are of the same type and they are equal')
+
+
+
 
