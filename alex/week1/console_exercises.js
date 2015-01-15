41cf9b38e56e096a0073caea28cbf6c42f96ea77
jswithalex <alexanderramsdell@gmail.com>
2015-01-15 17:05:11 -0500

    Update console_exercises.js

--- alex/week1/console_exercises.js
+++ alex/week1/console_exercises.js
@@ -1,30 +1,33 @@
 x
-VM240:2 Uncaught ReferenceError: x is not defined
+
 var x
-undefined
+
 var x = 15
-undefined
+
 var x = 4
-undefined
+
 Math.sqrt(x)
-2
+
 x
-4
+
+y = Math.sqrt(x)
+
 x = Math.sqrt(x)
-2
-alert('x is'+x)
-undefined
-console.log('x is' + x);
-VM408:2 x is2
-undefined
+
+alert('x is ' + x)
+
+console.log('x is ' + x);
+
 console.log('x + x = ' + x + x )
-VM454:2 x + x = 22
-undefined
+
 console.log('x + x = ' + 2x )
-VM239:762 Uncaught SyntaxError: Unexpected token ILLEGAL
+
 console.log('x + x = ' + 2*x )
-VM456:2 x + x = 4
-undefined
+
 console.log('x + x = ' + (x + x) )
-VM457:2 x + x = 4
-undefined
+
+'x'
+
+x
+
+
