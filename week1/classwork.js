9da231237d530a86ddf683f00498aa591234eec6
jswithalex <alexanderramsdell@gmail.com>
2015-01-18 12:21:23 -0500

    Update classwork.js

--- week1/classwork.js
+++ week1/classwork.js
@@ -1,17 +1,32 @@
+/*
+   Week 1 - Introduction to JavaScript Variables and the JavaScript Browser Console
+
+*/
+
+// this is a comment
+
+// this gives you an error because the JS environment doesn't know what x is.
 x
 
+// this is a variable declaration. It tells the JS environment to create a variable 'x' and keep track of it.
 var x
 
-var x = 15
+// this is a variable definition. It tells the JS environment that x is now 15.
+x = 15
 
+// this is an alternate way to declare and define a variable in one statement.
 var x = 4
 
+// here, we are using the Math library's square root function to get the sqrt of x.
 Math.sqrt(x)
 
+// but that function doesn't change x
 x
 
+// here, y holds the value of the square root of x
 y = Math.sqrt(x)
 
+// now we have x's square root stored in y
 y
 
 x = Math.sqrt(x)
