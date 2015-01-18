0ba2febd048689a220c7bc824baf0a5b7157f006
jswithalex <alexanderramsdell@gmail.com>
2015-01-18 12:29:46 -0500

    Update classwork.js

--- week1/classwork.js
+++ week1/classwork.js
@@ -1,6 +1,8 @@
 /*
    Week 1 - Introduction to JavaScript Variables and the JavaScript Browser Console
-
+   
+   note: When you enter things into the javascript console, it prints them out for you. If you're writing JS in a 
+   text file, you won't see the same behavior. You'll need to use alert() or console.log() to see text output.
 */
 
 // this is a comment
@@ -29,12 +31,10 @@ y = Math.sqrt(x)
 // now we have x's square root stored in y
 y
 
-x = Math.sqrt(x)
-
-x
-
+// this is a pop-up
 alert('x is ' + x)
 
+// this is how you log info to the console
 console.log('x is ' + x);
 
 console.log('2 times x is ' + 2x)
@@ -53,12 +53,25 @@ var fullName = 'alex' + 'ramsdell'
 
 var fullName = 'alex' + ' ' + 'ramsdell'
 
+var MAX = 12;
+
+if (fullName.length > MAX ) {
+    console.log('Your name is too long!')
+}
+
+else {
+  console.log('You are all set.')  
+}
+
 x = 4
 
 y = '4'
 
+// testing if x and y are the same, but not taking into account what type of variables they are
+// aka, converting them to the same type and only caring about their values
 y == x
 
+// this way only returns true if they are of the same type and if they are equal in value
 y === x
 
 y = x
@@ -71,5 +84,5 @@ if (x === y) {
 else {
   console.log('x and y are not equal'); 
 }
-}
+
 
