ea380a59cf4735439fa74a99f3ec5442979ad721
jswithalex <alexanderramsdell@gmail.com>
2015-01-18 12:12:15 -0500

    Update classwork.js

--- week1/classwork.js
+++ week1/classwork.js
@@ -1,3 +1,60 @@
-// console exercises
+x
 
-// javascript in html and in external files
+var x
+
+var x = 15
+
+var x = 4
+
+Math.sqrt(x)
+
+x
+
+y = Math.sqrt(x)
+
+y
+
+x = Math.sqrt(x)
+
+x
+
+alert('x is ' + x)
+
+console.log('x is ' + x);
+
+console.log('2 times x is ' + 2x)
+
+console.log('2 times x is ' + 2*x)
+
+console.log('x + x is ' + x + x )
+
+console.log('x + x = ' + (x + x) )
+
+var firstName = 'alex'
+
+var lastName = 'alex'
+
+var fullName = 'alex' + 'ramsdell'
+
+var fullName = 'alex' + ' ' + 'ramsdell'
+
+x = 4
+
+y = '4'
+
+y == x
+
+y === x
+
+y = x
+
+y === x
+
+if (x === y) {
+  console.log('x and y are of the same type and they are equal')
+}
+else {
+  console.log('x and y are not equal'); 
+}
+}
+
