7d26544d0e8d67d245ecb842ce5beb31064e17f3
jswithalex <alexanderramsdell@gmail.com>
2015-01-22 21:07:06 -0500

    Update and rename classwork.md to classwork.js

---
+++ week2/classwork.js
@@ -0,0 +1,34 @@
+// get a handle on each element of the form
+var form = document.getElementsByTagName('form');
+var username = document.getElementsByTagName('input')[0];
+var password = document.getElementsByTagName('input')[1];
+var password_confirm = document.getElementsByTagName('input')[2];
+var submitButton = document.getElementsByTagName('input')[3];
+var error_box = document.getElementsByTagName('p');
+
+// error message array
+var errors = [];
+var USER_MIN = 5;
+var PW_MIN = 8;
+
+submitButton.onsubmit = function(e) {
+
+
+  // what are our conditions for success?
+  // five or more chars in username
+  // eight or more chars in passwords
+  // passwords need to be identical
+  
+  if ( (username.value.length >= USER_MIN) && 
+       (password.value.length >= PW_MIN) && 
+       (password.value === password_confirm.value)
+     )
+  {
+  	 console.log('you submitted!');
+  }
+  
+  else {
+    e.preventDefault();
+  }
+
+}
--- week2/classwork.md
+++
@@ -1,1 +0,0 @@
-// will appear after week 2's class
