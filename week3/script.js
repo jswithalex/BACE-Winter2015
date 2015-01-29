4a61d411ab9181838f0f165c9fae280c182f70f5
jswithalex <alexanderramsdell@gmail.com>
2015-01-29 17:34:07 -0500

    Update script.js

--- week3/script.js
+++ week3/script.js
@@ -1,1 +1,6 @@
-/*  Image Gallery JavaScript goes here */
+/*  
+*  JavaScript Image Gallery
+*  note: make sure to download the images first!
+*  download them here: https://dl.dropboxusercontent.com/u/22170073/site/week3_imageGallery.zip
+*
+*/
