c4871d8ee4b12560189109ec2328113f5d08fd29
jswithalex <alexanderramsdell@gmail.com>
2015-01-29 18:07:09 -0500

    Update script.js

--- week3/script.js
+++ week3/script.js
@@ -1,6 +1,8 @@
 /*  
 *  JavaScript Image Gallery
 *  note: make sure to download the images first!
+*  
 *  download them here: https://dl.dropboxusercontent.com/u/22170073/site/week3_imageGallery.zip
-*
+*  make sure to put them in the Downloads/BACE-Winter2015-master/week3 folder
 */
+
