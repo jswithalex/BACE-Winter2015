0578deed86b003c01dfb8248328191e963924390
jswithalex <alexanderramsdell@gmail.com>
2015-02-05 18:03:37 -0500

    Update script.js

--- templates/script.js
+++ templates/script.js
@@ -1,1 +1,2 @@
-// put your javascript here.  the index.html file is already linked to this file, so you should be able to run it immediately if there aren't any errors
+// put your javascript here.  
+//the index.html file is already linked to this file, so you should be able to run it immediately if there aren't any errors
