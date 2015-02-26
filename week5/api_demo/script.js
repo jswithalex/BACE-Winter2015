26708604011986c821d162ce28d796289deb5d16
jswithalex <alexanderramsdell@gmail.com>
2015-02-26 15:12:52 -0500

    Update script.js

--- week5/api_demo/script.js
+++ week5/api_demo/script.js
@@ -1,3 +1,4 @@
+/*
   Weather API Demo using jQuery's getJSON method.
 
   + Get Location Data from your ip address
@@ -8,15 +9,22 @@
 
 // get ip location data
 $.getJSON('http://ip-api.com/json').then(function(locData) {
-  $('#city').html(locData.city);
-  $('#state').html(locData.regionName);
-  $('#country').html(locData.countryCode);
-  var weatherMapGETurl = 'http://openweathermap.org/data/2.5/weather?q=' + locData.city + ',' + locData.countryCode.toLowerCase() + '&APPID=<YOUR API KEY GOES HERE>&units=imperial'
+  // insert location data into the proper html elements
+  $('#city').text(locData.city);
+  $('#state').text(locData.regionName);
+  $('#country').text(locData.countryCode);
+  // build a url for requesting JSON data from the open weather map api
+  var weatherMapGETurl = 'http://openweathermap.org/data/2.5/weather?q=' + locData.city + ',' + locData.countryCode.toLowerCase() + '&APPID=YOUR_OPEN_WEATHER_MAP_API_KEY&units=imperial'
+  // we call the getJSON method again with the url we just built
+  // we also return the data passed back to the next function (in the 'then' function)
   return $.getJSON(weatherMapGETurl);
 }).then(function(weatherData) {
-  $('#temp').html(weatherData['main']['temp']);
-  $('#humidity').html(weatherData['main']['humidity']);
-  $('#description').html(weatherData['weather'][0]['description']);
+  // at this point, both api's should have returned their data. This is what 'then' ensures for us.
+  
+  // insert the weather into the HTML page
+  $('#temp').text(weatherData['main']['temp']);
+  $('#humidity').text(weatherData['main']['humidity']);
+  $('#description').text(weatherData['weather'][0]['description']);
   $('img').attr('src','http://openweathermap.org/img/w/'+ weatherData['weather'][0]['icon'] + '.png');
-  console.log('http://openweathermap.org/img/w/'+ weatherData['weather'][0]['icon'] + '.png');
+  
 });
