89ee176951adba40f7654a777a6217189301b167
jswithalex <alexanderramsdell@gmail.com>
2015-02-26 15:17:55 -0500

    Update script.js

--- week5/api_demo/script.js
+++ week5/api_demo/script.js
@@ -6,7 +6,7 @@
   + Get weather data using location data
   + insert that into the html
 */
-
+var API_KEY = '61beaf11b206ba345f116d0ebe6205c2';
 // get ip location data
 $.getJSON('http://ip-api.com/json').then(function(locData) {
   // insert location data into the proper html elements
@@ -14,17 +14,18 @@ $.getJSON('http://ip-api.com/json').then(function(locData) {
   $('#state').text(locData.regionName);
   $('#country').text(locData.countryCode);
   // build a url for requesting JSON data from the open weather map api
-  var weatherMapGETurl = 'http://openweathermap.org/data/2.5/weather?q=' + locData.city + ',' + locData.countryCode.toLowerCase() + '&APPID=YOUR_OPEN_WEATHER_MAP_API_KEY&units=imperial'
+  var weatherMapGETurl = 'http://openweathermap.org/data/2.5/weather?q=' + locData.city + ',' + locData.countryCode.toLowerCase() + '&APPID=YOUR_OPEN_WEATHER_MAP
+_API_KEY&units=imperial'
   // we call the getJSON method again with the url we just built
   // we also return the data passed back to the next function (in the 'then' function)
   return $.getJSON(weatherMapGETurl);
 }).then(function(weatherData) {
   // at this point, both api's should have returned their data. This is what 'then' ensures for us.
-  
+
   // insert the weather into the HTML page
   $('#temp').text(weatherData['main']['temp']);
   $('#humidity').text(weatherData['main']['humidity']);
   $('#description').text(weatherData['weather'][0]['description']);
   $('img').attr('src','http://openweathermap.org/img/w/'+ weatherData['weather'][0]['icon'] + '.png');
-  
+
 });
